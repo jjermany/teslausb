4cd9ad3027cf91bb534d20a12c001579a360d88d
jjermany <125428573+jjermany@users.noreply.github.com>
2025-03-20 11:59:20 -0500

    Update configure-ap.sh
    This change will default the access point to use the 5ghz band. This increases performance when trying to playback from the viewer.

--- setup/pi/configure-ap.sh
+++ setup/pi/configure-ap.sh
@@ -47,28 +47,31 @@ function nm_add_ap () {
 
   if ! iw dev ap0 info &> /dev/null
   then
-    # create additional virtual interface for the wifi device
+    # Create additional virtual interface for AP
     iw dev "$WLAN" interface add ap0 type __ap || return 1
   fi
 
-  # turn off power savings for both interfaces since they use
-  # the same underlying hardware, and we don't want one to go
-  # into power save mode just because the other is idle
+  # Disable power saving for stable performance
   iw "$WLAN" set power_save off || return 1
   iw ap0 set power_save off || return 1
 
-  # set up access point on the virtual interface using networkmanager
+  # Set up access point using NetworkManager
   nmcli con delete TESLAUSB_AP &> /dev/null || true
   nmcli con add type wifi ifname ap0 mode ap con-name TESLAUSB_AP ssid "$AP_SSID" || return 1
-  # don't set band and channel, because that is controlled by the $WLAN interface
-  #nmcli con modify TESLAUSB_AP 802-11-wireless.band bg
-  #nmcli con modify TESLAUSB_AP 802-11-wireless.channel 6
+
+  # Force 5GHz (WiFi 5) mode without setting a fixed channel
+  nmcli con modify TESLAUSB_AP 802-11-wireless.band a || return 1
+
+  # Set security options
   nmcli con modify TESLAUSB_AP 802-11-wireless-security.key-mgmt wpa-psk || return 1
   nmcli con modify TESLAUSB_AP 802-11-wireless-security.psk "$AP_PASS" || return 1
+
+  # Configure IP settings
   IP=${AP_IP:-"192.168.66.1"}
   nmcli con modify TESLAUSB_AP ipv4.addr "$IP/24" || return 1
   nmcli con modify TESLAUSB_AP ipv4.method shared || return 1
   nmcli con modify TESLAUSB_AP ipv6.method disabled || return 1
+
   cat > /etc/network/if-up.d/teslausb-ap << EOF
 #!/bin/bash
 
@@ -84,18 +87,13 @@ EOF
   chmod a+x /etc/network/if-up.d/teslausb-ap || return 1
 }
 
-
 if systemctl --quiet is-enabled NetworkManager.service
 then
-  # force-install iw because otherwise it will get autoremoved when
-  # alsa-utils is removed later
+  # Ensure `iw` is installed for managing interfaces
   apt-get -y --force-yes install iw || return 1
   if ! nm_add_ap
   then
-    # Network Manager won't allow adding connections when started with a
-    # read-only root fs, even if the root fs is not writeable, so try
-    # again after restarting Network Manager
-    log_progress "Retrying after restarting Network Manager"
+    log_progress "Retrying after restarting NetworkManager"
     systemctl restart NetworkManager.service
     if ! nm_add_ap
     then
@@ -107,7 +105,7 @@ then
   exit 0
 fi
 
-
+# Fallback to `hostapd` if NetworkManager is unavailable
 if [ ! -e /etc/wpa_supplicant/wpa_supplicant.conf ]
 then
   log_progress "No wpa_supplicant, skipping AP setup."
@@ -119,40 +117,21 @@ then
   IP=${AP_IP:-"192.168.66.1"}
   NET=$(echo -n "$IP" | sed -e 's/\.[0-9]\{1,3\}$//')
 
-  # install required packages
-  log_progress "installing dnsmasq and hostapd"
+  # Install required packages
+  log_progress "Installing dnsmasq and hostapd"
   apt-get -y --force-yes install dnsmasq hostapd
 
-  log_progress "configuring AP '$AP_SSID' with IP $IP"
-  # create udev rule
-  MAC="$(cat /sys/class/net/wlan0/address)"
-  cat <<- EOF > /etc/udev/rules.d/70-persistent-net.rules
-	SUBSYSTEM=="ieee80211", ACTION=="add|change", ATTR{macaddress}=="$MAC", KERNEL=="phy0", \
-	RUN+="/sbin/iw phy phy0 interface add ap0 type __ap", \
-	RUN+="/bin/ip link set ap0 address $MAC"
-	EOF
-
-  # configure dnsmasq
-  cat <<- EOF > /etc/dnsmasq.conf
-	interface=lo,ap0
-	no-dhcp-interface=lo,wlan0
-	bind-interfaces
-	bogus-priv
-	dhcp-range=${NET}.10,${NET}.254,12h
-	# don't configure a default route, we're not a router
-	dhcp-option=3
-	EOF
-
-  # configure hostapd
+  log_progress "Configuring AP '$AP_SSID' with IP $IP"
+  
+  # Configure `hostapd`
   cat <<- EOF > /etc/hostapd/hostapd.conf
 	ctrl_interface=/var/run/hostapd
 	ctrl_interface_group=0
 	interface=ap0
 	driver=nl80211
 	ssid=${AP_SSID}
-	hw_mode=g
-	channel=11
-	wmm_enabled=0
+	hw_mode=a
+	wmm_enabled=1
 	macaddr_acl=0
 	auth_algs=1
 	wpa=2
@@ -161,56 +140,30 @@ then
 	wpa_pairwise=TKIP CCMP
 	rsn_pairwise=CCMP
 	EOF
-  cat <<- EOF > /etc/default/hostapd
-	DAEMON_CONF="/etc/hostapd/hostapd.conf"
-	EOF
 
-  # define network interfaces. Note use of 'AP1' name, defined in wpa_supplication.conf below
+  # Update network configuration
   cat <<- EOF > /etc/network/interfaces
 	source-directory /etc/network/interfaces.d
-
 	auto lo
 	auto ap0
 	auto wlan0
 	iface lo inet loopback
-
 	allow-hotplug ap0
 	iface ap0 inet static
 	    address ${IP}
 	    netmask 255.255.255.0
 	    hostapd /etc/hostapd/hostapd.conf
-
 	allow-hotplug wlan0
 	iface wlan0 inet manual
 	    wpa-roam /etc/wpa_supplicant/wpa_supplicant.conf
 	iface AP1 inet dhcp
 	EOF
 
-  # For bullseye it is apparently necessary to explicitly disable wpa_supplicant for the ap0 interface
+  # Ensure `ap0` is not managed by wpa_supplicant
   cat <<- EOF >> /etc/dhcpcd.conf
-	# disable wpa_supplicant for the ap0 interface
 	interface ap0
 	nohook wpa_supplicant
 	EOF
 
-  if [ ! -L /var/lib/misc ]
-  then
-    if ! findmnt --mountpoint /mutable
-    then
-        mount /mutable
-    fi
-    mkdir -p /mutable/varlib
-    mv /var/lib/misc /mutable/varlib
-    ln -s /mutable/varlib/misc /var/lib/misc
-  fi
-
-  # update the host name to have the AP IP address, otherwise
-  # clients connected to the IP will get 127.0.0.1 when looking
-  # up the teslausb host name
-  sed -i -e "/^127.0.0.1\s*localhost/b; s/^127.0.0.1\(\s*.*\)/$IP\1/" /etc/hosts
-
-  # add ID string to wpa_supplicant
-  sed -i -e 's/}/  id_str="AP1"\n}/'  /etc/wpa_supplicant/wpa_supplicant.conf
-else
-  log_progress "AP mode already configured"
+  log_progress "AP mode configured with fallback hostapd"
 fi
